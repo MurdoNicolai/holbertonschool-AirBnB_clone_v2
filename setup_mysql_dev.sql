2b46166cfa45e9ba589b7ac495f621bd1eefb18b
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-23 13:04:50 +0100

    typo correct

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- a script that prepares a MySQL server for the project
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_deb;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 CREATE USER
 IF NOT EXISTS 'hbnb_dev'@'localhost'
