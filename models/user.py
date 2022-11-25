658d00d32d338601603e35c0766337e941481918
jonseb974 <5151@holbertonschool.com>
2022-11-25 17:31:45 +0100

    add data tablename_email_ password_firstname_lastname_in_user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import String, Column
+from models.base_model import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """This class defines a user by various attributes
+
+    Attributes:
+        __tablename__: represents the table name, users.
+        email: represents user email address.
+        first_name: The user first name.
+        last_name: The user last name.
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
