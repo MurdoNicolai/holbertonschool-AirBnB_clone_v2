ab6613ab723a4d08c1806c8a09c4ed455bcba5f8
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-11-26 21:44:53 +0100

    adding reviews

--- models/review.py
+++ models/review.py
@@ -10,14 +10,14 @@ from sqlalchemy.orm import relationship
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
-    place_id = Column(String(60), ForeignKey('place_id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('user_id'), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
 
     place = relationship("Place", back_populates="reviews")
     user = relationship("User", back_populates="reviews")
 
-Place.cities = relationship(
+Place.reviews = relationship(
     "Review", order_by=Review.id, back_populates="place")
-User.cities = relationship(
+User.reviews = relationship(
     "Review", order_by=Review.id, back_populates="user")
